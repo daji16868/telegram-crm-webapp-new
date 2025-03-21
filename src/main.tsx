716ca955f6305be3e6e14feeef651bc696cb5990
daji16868 <zhudan690@gmail.com>
2025-03-22 05:18:19 +0700

    添加src/main.tsx文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
-import { initTelegramApp } from './utils/telegram'
 
-// 初始化 Telegram WebApp
-initTelegramApp();
-
-// 渲染 React 应用
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
