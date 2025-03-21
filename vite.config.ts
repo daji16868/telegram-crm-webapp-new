118aa791de6d8340ae7f6ed5024c81f9fdc69b55
daji16868 <zhudan690@gmail.com>
2025-03-22 05:28:11 +0700

    添加vite.config.ts文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1600
-  }
+    sourcemap: process.env.NODE_ENV === 'development',
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: false,
+  },
 })
